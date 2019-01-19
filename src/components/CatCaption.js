986b6b7e8599b6ae08b0a307ac149e0dbb61060f
Bartek <aqfgit@gmail.com>
2019-01-19 19:29:41 +0100

    Create basic components

---
+++ src/components/CatCaption.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CatCaption extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default CatCaption
---
+++ src/components/CatsChangerButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CatsChangerButton extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default CatsChangerButton
